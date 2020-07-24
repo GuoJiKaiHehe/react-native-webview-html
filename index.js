56b7858a21ebc91706fca1af3b6da8ef6cc1dd2e
GuoJikaiHehe <970228812@qq.com>
2020-07-24 22:41:01 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,104 @@
+import React, {Component} from 'react';
+import {
+    ScrollView,
+    AsyncStorage,
+    NativeModules,
+    Text,
+    View,
+    ActivityIndicator,
+    Animated,
+    PushNotificationIOS,
+    Platform,
+    DeviceEventEmitter,
+    InteractionManager,
+    TouchableOpacity,
+    ImageBackground,
+     Image,
+    StyleSheet,
+} from 'react-native'
+import AutoHeightWebView from './lib/AutoHeightWebView';
+
+export default class WebViewHtmlView extends Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+			htmlHegiht:0,
+			isLoading:true,
+	    };	
+	}
+	static defaultProps = {
+	  onClickImg(){},
+	}
+	componentDidMount(){
+		
+	}
+
+	render(){
+		var content = this.props.content;
+		// console.log("content",content);
+		return (<AutoHeightWebView 
+			// onLoadStart={syntheticEvent => {
+	  //           // update component to be aware of loading status
+	  //               const { nativeEvent } = syntheticEvent;
+	  //               this.setState({
+	  //               	isLoading:nativeEvent.loading,
+	  //               })
+	  //           }}
+	  //           onLoadEnd={syntheticEvent => {
+		 //            // update component to be aware of loading status
+		 //            const { nativeEvent } = syntheticEvent;
+		 //            // this.isLoading = nativeEvent.loading;
+		 //             this.setState({
+	  //               	isLoading:nativeEvent.loading,
+	  //               });
+	  //         }}
+
+			source={{html:content}}
+			style={[{marginTop: 0 ,height:this.state.htmlHegiht||30,width:'100%'}]}
+			onSizeUpdated={(size)=>{
+		    	console.log("size",size)
+		    	if(size.height!=this.state.htmlHegiht){
+		    		console.log("size",size);
+			    	this.setState({
+			    		htmlHegiht:size.height,
+			    	});
+			    	
+		    	}
+		    }}
+		    startInLoadingState={true}
+		    renderLoading={()=>{
+		    	return <ActivityIndicator size= 'large'  />
+		    }}
+		    onClickImg={(data)=>{
+		    	// {urls:[],curIndex:0}
+		    	this.props.onClickImg && this.props.onClickImg(data);
+
+		    }}
+		    zoomable={false}
+		    scrollEnabledWithZoomedin={false}
+
+		    viewportContent={'viewport-fit=cover,user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'}
+		    // disableTouchHideKeyboard={true}
+		    customScript={`
+
+		    	`}
+		    scalesPageToFit={true}
+		    customStyle={`
+			     img{
+			     	max-width:100%!important;
+			     	height:auto!important;
+			     	object-fit:contain;
+
+			     }
+			     p{
+			     	text-align:justify;
+			     }
+			     body{
+
+			     }
+			    `}
+			/>)
+	}
+
+}
+
